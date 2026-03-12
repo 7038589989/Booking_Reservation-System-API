ac40a751c402d05f25d26fd4150dfee91077b69b
mohit bhombe <bhombemohit@gmail.com>
2026-03-12 21:26:01 +0530

    Init

---
+++ routes/resourceRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({ message: "Get all resources" });
+});
+
+router.post("/", (req, res) => {
+  res.json({ message: "Create resource" });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const resourceRoutes = require("./routes/resourceRoutes");
+
+
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/resources", resourceRoutes);
 
 app.get("/", (req, res) => {
     res.send("Booking System API is running...");
